19548668d9e39cabd07e3610135b8a5a1b00e2bf
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-04-10 12:36:44 +0200

    Add 'date' to metrics.groovy script.

--- scripts/metrics.groovy
+++ scripts/metrics.groovy
@@ -4,14 +4,15 @@ engine = VCSEngineBuilder
 	.ofGit("https://github.com/amaembo/streamex.git")
 	.build()
 
-println "id,LOC,SLOC,CLOC,NOT,SNOT,CNOT"
+println "id,date,LOC,SLOC,CLOC,NOT,SNOT,CNOT"
 engine.each {
 	size = FSTree.of(it.revision.files).computeSize()
-	println	"${it.revision.commitId}," +
-		"${size.LOC},"             +
-		"${size.SLOC},"            +
-		"${size.CLOC},"            +
-		"${size.NOT},"             +
-		"${size.SNOT},"            +
+	println	"${it.revision.id},"    +
+		"${it.commits[0].dateTime}" +
+		"${size.LOC},"              +
+		"${size.SLOC},"             +
+		"${size.CLOC},"             +
+		"${size.NOT},"              +
+		"${size.SNOT},"             +
 		"${size.CNOT}"
 }
