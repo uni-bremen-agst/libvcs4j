1ba25480bbf912455bd3f8e0006fc6cfdbc7c2d6
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-08-29 16:05:58 +0200

    Add script ownership.groovy.

---
+++ scripts/ownership.groovy
@@ -0,0 +1,31 @@
+@Grapes([
+		@Grab(group='de.uni-bremen.informatik.st', module='libvcs4j', version='1.2.0'),
+		@Grab(group='org.slf4j', module='slf4j-simple', version='1.7.25')
+])
+
+import de.unibremen.informatik.st.libvcs4j.*
+
+def vcs = VCSEngineBuilder
+		.ofGit("https://github.com/INRIA/spoon.git")
+		.build()
+
+def authors = new HashSet<String>()
+def rev2ownership = new LinkedHashMap<String, Map<String, Integer>>()
+
+vcs.forEach {
+	id = it.revision.id
+	rev2ownership.put(id, new HashMap<>())
+	it.revision.getFilesBySuffix(".java").forEach {
+		it.readLineInfo().forEach {
+			authors.add(it.author)
+			rev2ownership.get(id).merge(it.author, 1, Integer.&sum)
+		}
+	}
+}
+
+println("id,${String.join(',', authors)}")
+rev2ownership.forEach ({ id, ownership ->
+	print("${id}")
+	authors.forEach{ print(",${ownership.getOrDefault(it, 0)}") }
+	print('\n')
+})
