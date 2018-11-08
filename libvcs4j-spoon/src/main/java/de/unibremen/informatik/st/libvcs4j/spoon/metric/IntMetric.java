605832fc1d00837976b0ee3a3da112618f8634fd
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-11-08 10:43:41 +0100

    Add metric base classes.

---
+++ libvcs4j-spoon/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/metric/IntMetric.java
@@ -0,0 +1,19 @@
+package de.unibremen.informatik.st.libvcs4j.spoon.metric;
+
+import de.unibremen.informatik.st.libvcs4j.Validate;
+
+public class IntMetric extends Metric<Integer> {
+
+	@Override
+	protected Integer sum(final Integer a, final Integer b)
+			throws NullPointerException {
+		return Validate.notNull(a) + Validate.notNull(b);
+	}
+
+	/**
+	 * Increments the metric of the top element of {@link #stack} by 1.
+	 */
+	void inc() {
+		inc(1);
+	}
+}
