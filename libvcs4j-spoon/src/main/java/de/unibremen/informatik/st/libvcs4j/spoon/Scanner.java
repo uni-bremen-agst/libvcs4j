09d39395bd5c82a9f2dd096ba92dbb879ad43677
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2018-11-08 09:45:11 +0100

    Remove NullPointerException from isFieldAccess.

--- libvcs4j-spoon/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/Scanner.java
+++ libvcs4j-spoon/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/Scanner.java
@@ -9,6 +9,7 @@ import spoon.reflect.code.CtInvocation;
 import spoon.reflect.code.CtReturn;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.declaration.CtNamedElement;
 import spoon.reflect.reference.CtReference;
 import spoon.reflect.visitor.CtScanner;
 
@@ -123,16 +124,14 @@ public class Scanner extends CtScanner {
 	 * @return
 	 * 			{@code true} if {@code method} is a getter or setter function,
 	 * 			{@code false} otherwise.
-	 * @throws NullPointerException
-	 * 			If {@code method == null}.
 	 */
 	public static boolean isFieldAccess(final CtMethod method)
 			throws NullPointerException {
-		final String name = Validate.notNull(method).getSimpleName();
+		final String name = Optional.ofNullable(method)
+				.map(CtNamedElement::getSimpleName).orElse(null);
 		return resolveToFieldAccess(method).isPresent() ||
 				// Abstract method or interface.
 				name != null && name.matches(GETTER_SETTER_REGEX);
-
 	}
 
 	/**
