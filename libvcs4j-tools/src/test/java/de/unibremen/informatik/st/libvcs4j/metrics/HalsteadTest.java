4e065bb9e65e2c553a97330ed5f9057b042646aa
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-03-12 10:10:38 +0100

    Add more Halstead tests.

--- libvcs4j-tools/src/test/java/de/unibremen/informatik/st/libvcs4j/metrics/HalsteadTest.java
+++ libvcs4j-tools/src/test/java/de/unibremen/informatik/st/libvcs4j/metrics/HalsteadTest.java
@@ -15,60 +15,60 @@ public class HalsteadTest {
 			new Complexity.Halstead(12, 7, 27, 15);
 
 	@Test
-	public void halsteadVocabulary() {
+	public void vocabulary() {
 		assertThat(halstead.getVocabulary()).isEqualTo(19);
 	}
 
 	@Test
-	public void halsteadProgramLength() {
+	public void programLength() {
 		assertThat(halstead.getProgramLength()).isEqualTo(42);
 	}
 
 	@Test
-	public void halsteadVolume() {
+	public void volume() {
 		assertThat(halstead.getVolume()).isEqualTo(178.4, within(0.1));
 	}
 
 	@Test
-	public void halsteadDifficulty() {
+	public void difficulty() {
 		assertThat(halstead.getDifficulty()).isEqualTo(12.85, within(0.01));
 	}
 
 	@Test
-	public void halsteadProgramLevel() {
+	public void programLevel() {
 		assertThat(halstead.getProgramLevel()).isEqualTo(0.077, within(0.01));
 	}
 
 	@Test
-	public void halsteadEffort() {
+	public void effort() {
 		// within(2.0) is necessary due to double precision limits.
 		assertThat(halstead.getEffort()).isEqualTo(2292, within(2.0));
 	}
 
 	@Test
-	public void halsteadRequiredTime() {
+	public void requiredTime() {
 		assertThat(halstead.getRequiredTime()).isEqualTo(127.35, within(0.1));
 	}
 
 	@Test
-	public void halsteadBugs() {
+	public void bugs() {
 		assertThat(halstead.getBugs()).isEqualTo(0.05, within(0.01));
 	}
 
 	@Test
-	public void halsteadConstructor001() {
+	public void constructor001() {
 		thrown.expect(IllegalArgumentException.class);
 		new Complexity.Halstead(3, 0, 2, 0);
 	}
 
 	@Test
-	public void halsteadConstructor002() {
+	public void constructor002() {
 		thrown.expect(IllegalArgumentException.class);
 		new Complexity.Halstead(0, 6, 0, 2);
 	}
 
 	@Test
-	public void halsteadConstructor003() {
+	public void constructor003() {
 		Complexity.Halstead copy = new Complexity.Halstead(halstead);
 		assertThat(copy.getNumDistinctOperators())
 				.isEqualTo(halstead.getNumDistinctOperators());
@@ -81,12 +81,36 @@ public class HalsteadTest {
 	}
 
 	@Test
-	public void halsteadAdd() {
+	public void constructor004() {
+		thrown.expect(NullPointerException.class);
+		new Complexity.Halstead(null);
+	}
+
+	@Test
+	public void add() {
 		final Complexity.Halstead add =
 				halstead.add(new Complexity.Halstead(7, 7, 7, 7));
 		assertThat(add.getNumDistinctOperators()).isEqualTo(19);
 		assertThat(add.getNumDistinctOperands()).isEqualTo(14);
 		assertThat(add.getNumOperators()).isEqualTo(34);
 		assertThat(add.getNumOperands()).isEqualTo(22);
+	}
+
+	@Test
+	public void volumeOfEmpty() {
+		assertThat(Complexity.Halstead.EMPTY_HALSTEAD.getVocabulary())
+				.isEqualTo(0);
+	}
+
+	@Test
+	public void difficultyOfEmpty() {
+		assertThat(Complexity.Halstead.EMPTY_HALSTEAD.getDifficulty())
+				.isEqualTo(0);
+	}
+
+	@Test
+	public void programLevelOfEmpty() {
+		assertThat(Complexity.Halstead.EMPTY_HALSTEAD.getProgramLevel())
+				.isEqualTo(0);
 	}
 }
