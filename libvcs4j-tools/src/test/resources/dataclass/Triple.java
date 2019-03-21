a5152e27843a0818a712a19660b13c4dc50ad5e0
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-03-21 18:19:27 +0100

    Add DataClassDetector test.

---
+++ libvcs4j-tools/src/test/resources/dataclass/Triple.java
@@ -0,0 +1,30 @@
+public class Triple {
+
+	private int x;
+	private int y;
+	private String value;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
