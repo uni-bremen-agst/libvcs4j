f25b88c5b25a06773701598277c7fc1fd63f147a
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-03-21 17:13:09 +0100

    Fix WMC scanner.

--- libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/metric/WMC.java
+++ libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/metric/WMC.java
@@ -1,11 +1,9 @@
 package de.unibremen.informatik.st.libvcs4j.spoon.metric;
 
 import spoon.reflect.declaration.CtClass;
-import spoon.reflect.declaration.CtConstructor;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtEnum;
 import spoon.reflect.declaration.CtInterface;
-import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.declaration.CtType;
 
 import java.util.Optional;
@@ -70,21 +68,5 @@ public class WMC extends IntGatherer {
 		visitNode(ctEnum, super::visitCtEnum, (__, parent) -> parent,
 				mcc.metricOf(ctEnum).map(i -> i - 1)
 				.orElseThrow(IllegalStateException::new));
-	}
-
-	@Override
-	public <T> void visitCtMethod(final CtMethod<T> pMethod) {
-		if (!pMethod.isImplicit()) {
-			inc();
-		}
-		super.visitCtMethod(pMethod);
-	}
-
-	@Override
-	public <T> void visitCtConstructor(final CtConstructor<T> pConstructor) {
-		if (!pConstructor.isImplicit()) {
-			inc();
-		}
-		super.visitCtConstructor(pConstructor);
 	}
 }
