f00f76c65cd11c74647f783493961347c32d1067
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-04-24 12:01:39 +0200

    Declare loc, sloc, cloc, not, snot, and cnot on a separate line.

--- libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/metrics/Metrics.java
+++ libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/metrics/Metrics.java
@@ -50,7 +50,12 @@ public class Metrics {
 				.newLenientScanner(lang, content, "");
 
 		final String LINE_SEP = "\\r?\\n";
-		int loc = 0, sloc = 0, cloc = 0, not = 0, snot = 0, cnot = 0;
+		int loc  = 0;
+		int sloc = 0;
+		int cloc = 0;
+		int not  = 0;
+		int snot = 0;
+		int cnot = 0;
 		IToken lastToken = null;
 		try {
 			for (IToken token = scanner.getNextToken();
