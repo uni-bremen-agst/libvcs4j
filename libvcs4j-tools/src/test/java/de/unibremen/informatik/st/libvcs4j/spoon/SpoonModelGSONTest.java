4cce8eddf58fa287a9fb4f46f9a4d7182e7f3b93
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-05-03 13:14:18 +0200

    Add another test for SpoonModel.
    This test checks if a package is deleted although it still contains a
    package-info.java file

--- libvcs4j-tools/src/test/java/de/unibremen/informatik/st/libvcs4j/spoon/SpoonModelGSONTest.java
+++ libvcs4j-tools/src/test/java/de/unibremen/informatik/st/libvcs4j/spoon/SpoonModelGSONTest.java
@@ -1,19 +1,29 @@
 package de.unibremen.informatik.st.libvcs4j.spoon;
 
+import de.unibremen.informatik.st.libvcs4j.FileChange;
+import de.unibremen.informatik.st.libvcs4j.Revision;
 import de.unibremen.informatik.st.libvcs4j.RevisionRange;
 import de.unibremen.informatik.st.libvcs4j.VCSEngine;
 import de.unibremen.informatik.st.libvcs4j.VCSEngineBuilder;
+import de.unibremen.informatik.st.libvcs4j.VCSFile;
 import de.unibremen.informatik.st.libvcs4j.testutils.ResourceExtractor;
+import lombok.AllArgsConstructor;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class SpoonModelGSONTest {
 
@@ -44,6 +54,120 @@ public class SpoonModelGSONTest {
 		model.setIncremental(true);
 		for (RevisionRange range : engine) {
 			assertThat(model.update(range)).isNotNull();
+		}
+	}
+
+	@Test
+	public void nullPointerInUpdateFrom565To567() throws Exception {
+		VCSEngine engine = VCSEngineBuilder
+				.ofGit(repository.toString())
+				.withRoot("gson/src/main")
+				.withTarget(target.toAbsolutePath())
+				.withStartIdx(565)
+				.withEndIdx(566)
+				.build();
+
+		SpoonModel model = new SpoonModel();
+		model.setIncremental(true);
+
+		// To raise the NullPointerException deterministically, we need to
+		// delete the following java files one after another. Otherwise,
+		// 'package-info.java' may not be deleted at last.
+
+		Path output = engine.getOutput();
+		Path basePath = Paths.get("java", "com", "google", "gson", "internal",
+				"alpha");
+
+		RevisionRange r1 = engine.next().orElseThrow(AssertionError::new);
+		assertThat(model.update(r1)).isNotNull();
+		assertThat(model.getModel().orElseThrow(AssertionError::new)
+				.getAllPackages().stream()
+				.filter(pkg -> pkg.getQualifiedName().endsWith(
+						"com.google.gson.internal.alpha"))
+				.count()).isEqualTo(1);
+
+		RevisionRange r2 = mock(RevisionRange.class);
+		Path p2 = basePath.resolve("Intercept.java");
+		assertThat(output.resolve(p2).toFile().delete()).isTrue();
+		when(r2.getRevision()).thenReturn(r1.getRevision());
+		when(r2.getFileChanges()).thenReturn(singletonList(new RemoveMock(
+				r1.getRevision(), p2.toString())));
+		when(r2.getRemovedFiles()).thenCallRealMethod();
+		assertThat(model.update(r2)).isNotNull();
+		assertThat(model.getModel().orElseThrow(AssertionError::new)
+				.getAllPackages().stream()
+				.filter(pkg -> pkg.getQualifiedName().endsWith(
+						"com.google.gson.internal.alpha"))
+				.count()).isEqualTo(1);
+
+		RevisionRange r3 = mock(RevisionRange.class);
+		Path p3 = basePath.resolve("JsonPostDeserializer.java");
+		assertThat(output.resolve(p3).toFile().delete()).isTrue();
+		when(r3.getRevision()).thenReturn(r1.getRevision());
+		when(r3.getFileChanges()).thenReturn(singletonList(new RemoveMock(
+				r1.getRevision(), p3.toString())));
+		when(r3.getRemovedFiles()).thenCallRealMethod();
+		assertThat(model.update(r3)).isNotNull();
+		assertThat(model.getModel().orElseThrow(AssertionError::new)
+				.getAllPackages().stream()
+				.filter(pkg -> pkg.getQualifiedName().endsWith(
+						"com.google.gson.internal.alpha"))
+				.count()).isEqualTo(1);
+
+		RevisionRange r4 = mock(RevisionRange.class);
+		Path p4 = basePath.resolve("package-info.java");
+		assertThat(output.resolve(p4).toFile().delete()).isTrue();
+		when(r4.getRevision()).thenReturn(r1.getRevision());
+		when(r4.getFileChanges()).thenReturn(singletonList(new RemoveMock(
+				r1.getRevision(),
+				"java/com/google/gson/internal/alpha/package-info.java")));
+		when(r4.getRemovedFiles()).thenCallRealMethod();
+		assertThat(model.update(r4)).isNotNull();
+		assertThat(model.getModel().orElseThrow(AssertionError::new)
+				.getAllPackages().stream()
+				.filter(pkg -> pkg.getQualifiedName().endsWith(
+						"com.google.gson.internal.alpha"))
+				.count()).isZero();
+	}
+
+	@AllArgsConstructor
+	class RemoveMock implements FileChange {
+		private final Revision revision;
+		private final String relPath;
+
+		@Override
+		public Optional<VCSFile> getOldFile() {
+			return Optional.of(new VCSFile() {
+				@Override
+				public String getRelativePath() {
+					return relPath;
+				}
+
+				@Override
+				public Revision getRevision() {
+					return revision;
+				}
+
+				@Override
+				public Optional<Charset> guessCharset() {
+					return Optional.of(StandardCharsets.UTF_8);
+				}
+
+				@Override
+				public VCSEngine getVCSEngine() {
+					return revision.getVCSEngine();
+				}
+			});
+		}
+
+		@Override
+		public Optional<VCSFile> getNewFile() {
+			return Optional.empty();
+		}
+
+		@Override
+		public VCSEngine getVCSEngine() {
+			return revision.getVCSEngine();
 		}
 	}
 }
