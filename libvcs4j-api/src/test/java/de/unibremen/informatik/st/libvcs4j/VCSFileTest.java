6fe7cf83106bcb5f2e2c4ab676c49a41096d18e0
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2019-05-13 15:03:02 +0200

    Enhance mock in VCSFileTest.

--- libvcs4j-api/src/test/java/de/unibremen/informatik/st/libvcs4j/VCSFileTest.java
+++ libvcs4j-api/src/test/java/de/unibremen/informatik/st/libvcs4j/VCSFileTest.java
@@ -3,9 +3,11 @@ package de.unibremen.informatik.st.libvcs4j;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -14,11 +16,8 @@ import static org.mockito.Mockito.when;
 public class VCSFileTest {
 
 	@Test
-	public void readLinesEOLUnixEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first line\nsecond line\n");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
+	public void readLinesWithEOLUnixEOL() throws IOException {
+		VCSFile file = new VCSFileMock("first line\nsecond line\n");
 
 		List<String> lines = file.readLinesWithEOL();
 		assertThat(lines)
@@ -27,11 +26,8 @@ public class VCSFileTest {
 	}
 
 	@Test
-	public void readLinesEOLWindowsEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first line\r\nsecond line\r\n");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
+	public void readLinesWithEOLWindowsEOL() throws IOException {
+		VCSFile file = new VCSFileMock("first line\r\nsecond line\r\n");
 
 		List<String> lines = file.readLinesWithEOL();
 		assertThat(lines)
@@ -40,11 +36,8 @@ public class VCSFileTest {
 	}
 
 	@Test
-	public void readLinesEOLOldMacEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first line\rsecond line\r");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
+	public void readLinesWithEOLOldMacEOL() throws IOException {
+		VCSFile file = new VCSFileMock("first line\rsecond line\r");
 
 		List<String> lines = file.readLinesWithEOL();
 		assertThat(lines)
@@ -53,11 +46,8 @@ public class VCSFileTest {
 	}
 
 	@Test
-	public void readLinesEOLMixedEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first\rsecond\r\nthird\n");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
+	public void readLinesWithEOLMixedEOL() throws IOException {
+		VCSFile file = new VCSFileMock("first\rsecond\r\nthird\n");
 
 		List<String> lines = file.readLinesWithEOL();
 		assertThat(lines)
@@ -66,11 +56,8 @@ public class VCSFileTest {
 	}
 
 	@Test
-	public void readLinesEOLWithoutLastEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"foo\nbar");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
+	public void readLinesWithEOLWithoutLastEOL() throws IOException {
+		VCSFile file = new VCSFileMock("foo\nbar");
 
 		List<String> lines = file.readLinesWithEOL();
 		assertThat(lines)
@@ -79,10 +66,8 @@ public class VCSFileTest {
 	}
 
 	@Test
-	public void readLinesEOLEmptyContent() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn("");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
+	public void readLinesWithEOLEmptyContent() throws IOException {
+		VCSFile file = new VCSFileMock("");
 
 		List<String> lines = file.readLinesWithEOL();
 		assertThat(lines).isEmpty();
@@ -90,10 +75,7 @@ public class VCSFileTest {
 
 	@Test
 	public void readLinesMixedEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first\rsecond\r\nthird\n");
-		when(file.readLines()).thenCallRealMethod();
+		VCSFile file = new VCSFileMock("first\rsecond\r\nthird\n");
 
 		List<String> lines = file.readLines();
 		assertThat(lines)
@@ -114,13 +96,7 @@ public class VCSFileTest {
 
 	@Test
 	public void positionOfOffset() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first line\nsecond line");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
-		when(file.positionOf(4, 4)).thenCallRealMethod();
-		when(file.positionOf(14, 4)).thenCallRealMethod();
-		when(file.positionOf(11, 4)).thenCallRealMethod();
+		VCSFile file = new VCSFileMock("first line\nsecond line");
 
 		VCSFile.Position p1 = file.positionOf(4, 4)
 				.orElseThrow(AssertionError::new);
@@ -146,25 +122,13 @@ public class VCSFileTest {
 
 	@Test
 	public void positionOfOffsetEmptyString() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn("");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
-		when(file.positionOf(1, 4)).thenCallRealMethod();
-
+		VCSFile file = new VCSFileMock("");
 		assertThat(file.positionOf(1, 4)).isEmpty();
 	}
 
 	@Test
 	public void positionOfOffsetInEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first\nsecond\rthird\r\nforth");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
-
-		when(file.positionOf(5, 4)).thenCallRealMethod();
-		when(file.positionOf(12, 4)).thenCallRealMethod();
-		when(file.positionOf(18, 4)).thenCallRealMethod();
-		when(file.positionOf(19, 4)).thenCallRealMethod();
+		VCSFile file = new VCSFileMock("first\nsecond\rthird\r\nforth");
 
 		assertThat(file.positionOf(5, 4)).isEmpty();
 		assertThat(file.positionOf(12, 4)).isEmpty();
@@ -174,15 +138,7 @@ public class VCSFileTest {
 
 	@Test
 	public void positionOfLineAndColumn() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first line\nsecond line");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
-
-		when(file.positionOf(1, 6, 4)).thenCallRealMethod();
-		when(file.positionOf(2, 2, 4)).thenCallRealMethod();
-		when(file.positionOf(1, 11, 4)).thenCallRealMethod();
-		when(file.positionOf(2, 12, 4)).thenCallRealMethod();
+		VCSFile file = new VCSFileMock("first line\nsecond line");
 
 		VCSFile.Position p1 = file.positionOf(1, 6, 4)
 				.orElseThrow(AssertionError::new);
@@ -203,12 +159,8 @@ public class VCSFileTest {
 	}
 
 	@Test
-	public void positionOfLienAndColumnEmptyString() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn("");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
-		when(file.positionOf(1, 1, 4)).thenCallRealMethod();
-
+	public void positionOfLineAndColumnEmptyString() throws IOException {
+		VCSFile file = new VCSFileMock("");
 		assertThat(file.positionOf(1, 1, 4)).isEmpty();
 	}
 
@@ -219,24 +171,24 @@ public class VCSFileTest {
 				"foo\tbar\tfoobar");
 		when(file.readLinesWithEOL()).thenCallRealMethod();
 
-		when(file.positionOf(1, 8, 4)).thenCallRealMethod();
-		when(file.positionOf(1, 12, 8)).thenCallRealMethod();
+		when(file.positionOf(1, 9, 4)).thenCallRealMethod();
+		when(file.positionOf(1, 13, 8)).thenCallRealMethod();
 		when(file.positionOf(1, 1, 1)).thenCallRealMethod();
 		when(file.positionOf(1, 2, 1)).thenCallRealMethod();
 		when(file.positionOf(1, 3, 1)).thenCallRealMethod();
-		when(file.positionOf(1, 12, 2)).thenCallRealMethod();
+		when(file.positionOf(1, 13, 2)).thenCallRealMethod();
 
-		VCSFile.Position p1 = file.positionOf(1, 8, 4)
+		VCSFile.Position p1 = file.positionOf(1, 9, 4)
 				.orElseThrow(AssertionError::new);
 		assertThat(p1.getLine()).isEqualTo(1);
-		assertThat(p1.getColumn()).isEqualTo(8);
+		assertThat(p1.getColumn()).isEqualTo(9);
 		assertThat(p1.getTabSize()).isEqualTo(4);
 		assertThat(p1.getOffset()).isEqualTo(5);
 
-		VCSFile.Position p2 = file.positionOf(1, 12, 8)
+		VCSFile.Position p2 = file.positionOf(1, 13, 8)
 				.orElseThrow(AssertionError::new);
 		assertThat(p2.getLine()).isEqualTo(1);
-		assertThat(p2.getColumn()).isEqualTo(12);
+		assertThat(p2.getColumn()).isEqualTo(13);
 		assertThat(p2.getTabSize()).isEqualTo(8);
 		assertThat(p2.getOffset()).isEqualTo(5);
 
@@ -261,29 +213,66 @@ public class VCSFileTest {
 		assertThat(p5.getTabSize()).isEqualTo(1);
 		assertThat(p5.getOffset()).isEqualTo(2);
 
-		VCSFile.Position p6 = file.positionOf(1, 12, 2)
+		VCSFile.Position p6 = file.positionOf(1, 13, 2)
 				.orElseThrow(AssertionError::new);
 		assertThat(p6.getLine()).isEqualTo(1);
-		assertThat(p6.getColumn()).isEqualTo(12);
+		assertThat(p6.getColumn()).isEqualTo(13);
 		assertThat(p6.getTabSize()).isEqualTo(2);
 		assertThat(p6.getOffset()).isEqualTo(11);
 	}
 
 	@Test
 	public void positionOfLineAndColumnInEOL() throws IOException {
-		VCSFile file = mock(VCSFile.class);
-		when(file.readeContent()).thenReturn(
-				"first\nsecond\rthird\r\nforth");
-		when(file.readLinesWithEOL()).thenCallRealMethod();
-
-		when(file.positionOf(1, 6, 4)).thenCallRealMethod();
-		when(file.positionOf(2, 7, 4)).thenCallRealMethod();
-		when(file.positionOf(3, 6, 4)).thenCallRealMethod();
-		when(file.positionOf(3, 7, 4)).thenCallRealMethod();
+		VCSFile file = new VCSFileMock("first\nsecond\rthird\r\nforth");
 
 		assertThat(file.positionOf(1, 6, 4)).isEmpty();
 		assertThat(file.positionOf(2, 7, 4)).isEmpty();
 		assertThat(file.positionOf(3, 6, 4)).isEmpty();
 		assertThat(file.positionOf(3, 7, 4)).isEmpty();
+	}
+
+	@Test
+	public void positionOfLineAndColumnBugfix001() throws IOException {
+		VCSFile file = new VCSFileMock(
+				"\t\t\t\t\t\tif (!method.getParameters().get(i).getType().equals(ctMethod.getParameters().get(i).getType())) {\n");
+
+		VCSFile.Position position = file.positionOf(1, 118, 4)
+				.orElseThrow(AssertionError::new);
+		assertThat(position.getLine()).isEqualTo(1);
+		assertThat(position.getColumn()).isEqualTo(118);
+		assertThat(position.getTabSize()).isEqualTo(4);
+	}
+
+	private static class VCSFileMock implements VCSFile {
+		private final String content;
+
+		public VCSFileMock(final String content) {
+			this.content = Validate.notNull(content);
+		}
+
+		@Override
+		public Optional<Charset> guessCharset() {
+			return Optional.of(StandardCharsets.UTF_8);
+		}
+
+		@Override
+		public byte[] readAllBytes() {
+			return content.getBytes(StandardCharsets.UTF_8);
+		}
+
+		@Override
+		public String getRelativePath() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public Revision getRevision() {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public VCSEngine getVCSEngine() {
+			throw new UnsupportedOperationException();
+		}
 	}
 }
