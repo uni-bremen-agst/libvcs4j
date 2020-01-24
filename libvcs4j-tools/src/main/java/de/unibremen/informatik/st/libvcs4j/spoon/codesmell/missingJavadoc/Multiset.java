07f19a2ad2af8d8bd544970cd33c010f970bbaa4
Ruben Smidt <ruben@smidt-bremen.de>
2020-01-24 20:31:53 +0100

    Add Javadoc to multimap and multiset

--- libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/codesmell/missingJavadoc/Multiset.java
+++ libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/spoon/codesmell/missingJavadoc/Multiset.java
@@ -2,6 +2,16 @@ package de.unibremen.informatik.st.libvcs4j.spoon.codesmell.missingJavadoc;
 
 import java.util.Collection;
 
+/**
+ * Multiset is a google guava inspired collection that allows a set to contain duplicates.
+ *
+ * @param <V> the type of the values.
+ */
 public interface Multiset<V> extends Collection<V> {
+    /**
+     * Returns the count of a specific value in the set.
+     *
+     * @param value the value which occurrences have to be counted.
+     */
     int count(V value);
 }
