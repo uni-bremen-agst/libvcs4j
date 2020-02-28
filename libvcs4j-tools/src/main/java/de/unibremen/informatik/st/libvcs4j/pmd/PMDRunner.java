0a16e1ca933f7f33474390586e07b17478c8dc48
Marcel Steinbeck <marcel@informatik.uni-bremen.de>
2020-02-28 09:23:51 +0100

    Create a single cache file.

--- libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/pmd/PMDRunner.java
+++ libvcs4j-tools/src/main/java/de/unibremen/informatik/st/libvcs4j/pmd/PMDRunner.java
@@ -2,7 +2,9 @@ package de.unibremen.informatik.st.libvcs4j.pmd;
 
 import de.unibremen.informatik.st.libvcs4j.Revision;
 import de.unibremen.informatik.st.libvcs4j.Validate;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.sourceforge.pmd.PMD;
 import org.xml.sax.SAXException;
@@ -18,6 +20,7 @@ import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -30,9 +33,9 @@ import java.util.stream.Collectors;
 public class PMDRunner {
 
 	/**
-	 * List of default categories.
+	 * List of default rules.
 	 */
-	private static final List<String> DEFAULT_CATEGORIES = List.of(
+	private static final List<String> DEFAULT_RULES = List.of(
 			"category/java/design.xml/GodClass",
 			"category/java/design.xml/ExcessiveClassLength",
 			"category/java/design.xml/ExcessiveMethodLength",
@@ -43,15 +46,27 @@ public class PMDRunner {
 			"category/java/bestpractices.xml/UnusedPrivateMethod");
 
 	/**
-	 * The PMD rules to apply.
+	 * PMD rules to apply.
 	 */
 	private final List<String> rules;
 
 	/**
+	 * PMD cache file (incremental analysis). Is set lazily.
+	 */
+	private Path cacheFile = null;
+
+	/**
+	 * Enable (or disable) incremental analysis.
+	 */
+	@Getter
+	@Setter
+	private boolean incremental = true;
+
+	/**
 	 * Creates a new runner with given PMD rules. {@code null} values in
 	 * {@code rules} are filtered out. Non-Null values are trimmed with
-	 * {@link String#trim()}. If {@code rules} is empty, the ruleset
-	 * "rulesets/java/basic.xml" is used as fallback.
+	 * {@link String#trim()}. If {@code rules} is empty, the rules listed in
+	 * {@link #DEFAULT_RULES} are used.
 	 *
 	 * @param rules
 	 * 		The PMD rules to apply.
@@ -65,7 +80,7 @@ public class PMDRunner {
 				.map(String::trim)
 				.collect(Collectors.toList());
 		if (this.rules.isEmpty()) {
-			this.rules.addAll(DEFAULT_CATEGORIES);
+			this.rules.addAll(DEFAULT_RULES);
 		}
 	}
 
@@ -101,21 +116,34 @@ public class PMDRunner {
 			throws IOException {
 		Validate.validateState(!rules.isEmpty());
 
-		final Path cache = Files.createTempFile("libvcs4j-pmd", null);
-		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-			log.info("Deleting PMD cache file '{}'", cache);
-			try {
-				Files.delete(cache);
-			} catch (IOException e) {
-				log.warn("Error while deleting PMD cache", e);
-			}
-		}));
-		final String[] args = {
-				"-d", revision.getOutput().toString(), // input
-				"-f", "xml",                           // format
-				"-R", String.join(",", rules),         // rules
-				"-cache", cache.toString()
-		};
+		if (incremental && cacheFile == null) {
+			log.info("Creating PMD cache file");
+			cacheFile = Files.createTempFile("libvcs4j-pmd", null)
+					.toAbsolutePath();
+			log.info("Cache file is '{}'", cacheFile);
+			Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+				log.info("Deleting PMD cache file '{}'", cacheFile);
+				try {
+					Files.delete(cacheFile);
+				} catch (final IOException e) {
+					log.warn("Error while deleting PMD cache", e);
+				}
+			}));
+		}
+		final List<String> args = new ArrayList<>();
+		// input
+		args.add("-d");
+		args.add(revision.getOutput().toString());
+		// format
+		args.add("-f");
+		args.add("xml");
+		// rules
+		args.add("-R");
+		args.add(String.join(",", rules));
+		if (incremental) {
+			args.add("-cache");
+			args.add(cacheFile.toString());
+		}
 
 		// Temporarily redirect stdout to a string.
 		final PrintStream stdout = System.out;
@@ -123,7 +151,7 @@ public class PMDRunner {
 		final PrintStream ps = new PrintStream(bos);
 		System.setOut(ps);
 		try {
-			PMD.run(args);
+			PMD.run(args.toArray(String[]::new));
 			// According to PMD the resulting xml is UTF-8 encoded.
 			final String output = bos.toString(StandardCharsets.UTF_8.name());
 
